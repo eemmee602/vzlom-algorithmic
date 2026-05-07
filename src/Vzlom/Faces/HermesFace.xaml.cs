4f18b2b7891e7b8537369699d65d090f52f50bc7
eemmee602 <egirouxlafontaine@gmail.com>
2026-05-07 18:23:10 -0400

    fix: add missing using for BitmapSource

--- src/Vzlom/Faces/HermesFace.xaml.cs
+++ src/Vzlom/Faces/HermesFace.xaml.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Vzlom.Core;
 using Vzlom.Models;
 
